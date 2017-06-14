05286a4709408a545c73a99fe19c5442a213d637
zhonglin717 <717543275@qq.com>
2017-06-14 11:35:34 +0800

    集成cxf

---
+++ Jersey/src/main/java/com/pingan/cxf/HelloWorldService.java
@@ -0,0 +1,48 @@
+package com.pingan.cxf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebService;
+
+import com.pingan.entity.User;
+/**
+ * cxf  服务 实现  2017-6-13 16:05:28
+ * @author Administrator
+ * http://localhost:8080/Jersey/service/hellow?wsdl
+ *
+ */
+@WebService(endpointInterface="com.pingan.cxf.IHelloWorldService")//endpointInterface是为了在实现多个接口情况下指明webservice的接口
+public class HelloWorldService implements IHelloWorldService {
+
+	@Override
+	public List<User> query(String name) {
+		List<User> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			User user = new User();
+			user.setName("钟林"+i);
+			user.setAddress("湖北十堰"+i);
+			list.add(user);
+		}
+		return list;
+	}
+
+	@Override
+	public void add(User user) {
+		System.out.println(user.getName());
+		
+	}
+
+	@Override
+	public void delete(String id) {
+		System.out.println(id);
+		
+	}
+
+	@Override
+	public Integer update(User user) {
+		return 1;
+	}
+
+   
+}
---
+++ Jersey/src/main/java/com/pingan/cxf/IHelloWorldService.java
@@ -0,0 +1,21 @@
+package com.pingan.cxf;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import com.pingan.entity.User;
+@WebService
+public interface IHelloWorldService {
+
+	List<User> query(String name);
+
+	void add(User user);
+
+	void delete(String id);
+
+	Integer update(User user);
+	
+	
+
+}

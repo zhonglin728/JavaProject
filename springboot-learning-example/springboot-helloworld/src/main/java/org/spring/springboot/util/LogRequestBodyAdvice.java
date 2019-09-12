743cac5f339a44dec2a17f1aa97a4d94cf84880a
zhonglin <zhonglin@szhcf.com.cn>
2019-09-12 14:34:18 +0800

    参数拦截

---
+++ springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/util/LogRequestBodyAdvice.java
@@ -0,0 +1,11 @@
+package org.spring.springboot.util;
+
+/**
+ * @ClassName LogRequestBodyAdvice
+ * @Description: TODO
+ * @Author zhonglin
+ * @Date 2019/9/12
+ * @Version V1.0
+ **/
+public class LogRequestBodyAdvice {
+}
---
+++ springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/util/LogResponseBodyAdvice.java
@@ -0,0 +1,11 @@
+package org.spring.springboot.util;
+
+/**
+ * @ClassName LogResponseBodyAdvice
+ * @Description: TODO
+ * @Author zhonglin
+ * @Date 2019/9/12
+ * @Version V1.0
+ **/
+public class LogResponseBodyAdvice {
+}

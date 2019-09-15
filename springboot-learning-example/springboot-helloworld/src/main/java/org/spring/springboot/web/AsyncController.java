87904d45d01e7a4f6b10d27d33ece91a070212db
钟小懒 <717543275@qq.com>
2019-09-15 22:04:52 +0800

    是多少速度

--- springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/web/AsyncController.java
+++ springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/web/AsyncController.java
@@ -49,18 +49,18 @@ public class AsyncController {
 
             // Wait until they are all done
             //join() 的作用：让“主线程”等待“子线程”结束之后才能继续运行
-            CompletableFuture.allOf(page1,page2,page3).join();
+            CompletableFuture.allOf(page1,page2,page3,page4,page5,page6).join();
             //log.info("--> " + page1.get());
             //log.info("--> " + page2.get());
             //log.info("--> " + page3.get());
             //等待任务完成！
-            for(;;){
+           /* for(;;){
                 if (page1.isDone() && page2.isDone() && page3.isDone() && page4.isDone() && page5.isDone() && page6.isDone()){
                     break;
                 }
                 Thread.sleep(100);
                 log.info("等待ing!.......");
-            }
+            }*/
             float exc = (float)(System.currentTimeMillis() - start)/1000;
             log.info("Elapsed time: " + exc + " seconds");
 

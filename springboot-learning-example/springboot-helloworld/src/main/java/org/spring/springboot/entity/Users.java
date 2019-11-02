9a8499e6fb2cd8e3d3440bf71e66007ce885061c
钟小懒 <717543275@qq.com>
2019-11-02 16:14:00 +0800

    Revert "三十多岁所多"
    This reverts commit c6b9a7dcf4f443aed76eee411fb2044dd681ca41.

--- springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/entity/Users.java
+++ springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/entity/Users.java
@@ -19,7 +19,6 @@ import java.util.List;
 @NoArgsConstructor
 @Builder(toBuilder = true)
 public class Users extends BaseEntity{
-
     private Integer userId;
     private Integer deptId;
     private String loginName;

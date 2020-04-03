59ef382c68a882b864e427830a48115340f8122d
钟林 <717543275@qq.com>
2020-04-03 19:52:30 +0800

    2020年4月3日19:52:24

--- cloudDemo-master/feign/src/main/java/com/cloud/feign/rpc/FeiyanApi.java
+++ cloudDemo-master/feign/src/main/java/com/cloud/feign/rpc/FeiyanApi.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
-@FeignClient(name = "a",url = "http://www.dzyong.top:3005")
+@FeignClient(name = "a",url = "${api.feiyanUrl}")
 public interface FeiyanApi {
     @GetMapping(value = "yiqing/news",name = "获取肺炎新闻信息")
     //@Headers({"Content-Type: application/json","Accept: application/json"})

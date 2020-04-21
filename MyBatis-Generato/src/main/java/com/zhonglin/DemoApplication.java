7c6bba59569e8129e09694a76b3f1a2ebd8325e7
zhonglin <717543275@qq.com>
2020-04-21 12:07:30 +0800

    Mybatis   代码生成器

---
+++ MyBatis-Generato/src/main/java/com/zhonglin/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.zhonglin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhonglin
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}

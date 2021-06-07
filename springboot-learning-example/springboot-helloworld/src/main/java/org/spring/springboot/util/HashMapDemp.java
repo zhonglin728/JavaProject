ae126f52036737da4245be4a58086519a66ce60c
zhonglin <717543275@qq.com>
2021-06-07 17:13:33 +0800

    2021年6月7日17:13:26

---
+++ springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/util/HashMapDemp.java
@@ -0,0 +1,12 @@
+package org.spring.springboot.util;
+
+/**
+ * @author zhonglin
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/6/717:11
+ */
+public class HashMapDemp {
+
+}
--- springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/util/sortTest.java
+++ springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/util/sortTest.java
@@ -1,8 +1,22 @@
 package org.spring.springboot.util;
 
+import ch.qos.logback.core.util.FileUtil;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.FileSystemUtils;
 
+import java.io.File;
+import java.lang.reflect.Field;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 /**
@@ -14,6 +28,7 @@ public class sortTest {
 
 
     public static void main(String[] args) throws Exception {
+
         Integer [] a = {34,56,1,2,78,24,999,444,332,0,234,57,78,40,29};
         for (int i=0;i<a.length-1;i++) {
             for(int j=0;j<a.length-1-i;j++){
@@ -41,7 +56,18 @@ public class sortTest {
         List<String> strings = Arrays.asList(array);
 
         int i = friSum(4);
-        System.out.println(i);
+        System.out.println("---------------------------------------------------------");
+        File file = Paths.get("D:\\gitProject\\JavaProject\\springboot-learning-example\\springboot-helloworld\\target\\classes\\org\\spring\\springboot\\entity\\Dept.class").toFile();
+
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+        concurrentHashMap.put("a",1);
+        HashMap<Object, Object> objectObjectHashMap = Maps.newHashMap();
+        objectObjectHashMap.put("b",3);
+        CopyOnWriteArrayList<Object> objects = new CopyOnWriteArrayList<>();
+        objects.add(2);
+        int i1 = "钟林".hashCode();
+        System.out.println(i1);
+
     }
 
 

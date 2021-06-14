07399ec818357cf694c8858dc917d481df6ab4a0
zhonglin <717543275@qq.com>
2021-06-14 16:10:22 +0800

    nacos  2021年6月14日16:10:14

---
+++ nacosDemo/nacos-web/src/main/java/com/huawei/controller/UserSerivce.java
@@ -0,0 +1,21 @@
+package com.huawei.controller;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * @author zhonglin
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/6/1416:02
+ */
+@Service
+public class UserSerivce {
+    public Map my(){
+        return ImmutableMap.of("a",1);
+    }
+}
+
--- springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/util/HashMapDemp.java
+++ springboot-learning-example/springboot-helloworld/src/main/java/org/spring/springboot/util/HashMapDemp.java
@@ -1,5 +1,17 @@
 package org.spring.springboot.util;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jni.Thread;
+import sun.plugin.ClassLoaderInfo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author zhonglin
  * @Title:
@@ -7,6 +19,58 @@ package org.spring.springboot.util;
  * @Description:
  * @date 2021/6/717:11
  */
-public class HashMapDemp {
+@Slf4j
+public class HashMapDemp{
+    public static Map<String, Object> hashMap = new HashMap<String,Object>();
 
+    public static ReentrantLock reentrantLock = new ReentrantLock();
+
+    public static void main(String[] args) throws InterruptedException {
+        new java.lang.Thread(new Thread1()).start();
+        new java.lang.Thread(new Thread1()).start();
+        new java.lang.Thread(new Thread1()).start();
+        new java.lang.Thread(new Thread1()).start();
+
+
+    }
+
+    public static class  Thread1 implements Runnable{
+        public  int count = 100;
+        @Override
+        public synchronized void run() {
+                    //hashMap.put(String.valueOf(i)+"-T1",i);
+            while (true) {
+                count --;
+                log.info("剩余："+count);
+                if (count == 0) {
+                    break;
+                }
+            }
+
+        }
+    }
+
+    public static class Thread2 implements Runnable{
+        @Override
+        public  void run() {
+                for (int i=0;i<10;i++){
+                }
+        }
+    }
+
+    public static class Thread3 implements Runnable{
+        @Override
+        public  void run() {
+            for (int i=0;i<10;i++){
+            }
+        }
+    }
+    public static class Thread4 implements Runnable{
+        @Override
+        public  void run() {
+            for (int i=0;i<10;i++){
+
+            }
+        }
+    }
 }
